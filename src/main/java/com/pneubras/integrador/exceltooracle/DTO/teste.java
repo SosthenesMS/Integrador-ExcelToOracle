e85668525bf18dcdda50bfe20d0d63e4020f0032
SosthenesMS <sosthenesms@gmail.com>
2023-10-18 17:37:40 -0300

    Atualização

--- src/main/java/com/pneubras/integrador/exceltooracle/DTO/teste.java
+++ src/main/java/com/pneubras/integrador/exceltooracle/DTO/teste.java
@@ -18,19 +18,20 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import com.pneubras.integrador.exceltooracle.Entities.PlanilhaPrice;
 
 public class teste {
-	
-	Integer linha = 0;
-	Integer colunaA = 0;
-	Integer colunaB = 1;
-	Integer colunaC = 2;
-	Integer colunaD = 3;
-	Integer colunaE = 4;
+
+	static Integer linha = 0;
+	static Integer colunaA = 0;
+	static Integer colunaB = 1;
+	static Integer colunaC = 2;
+	static Integer colunaD = 3;
+	static Integer colunaE = 4;
 
 	public static void main(String[] args) throws IOException {
 		
 
-
-		List<PlanilhaPrice> planilhaExcel = new ArrayList<>();
+		//PlanilhaPrice planilhaPrice
+		List<Double> planilhaList = new ArrayList<>();
+		
 
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 		LocalDateTime time = LocalDateTime.now();
@@ -49,25 +50,27 @@ public class teste {
 		HSSFSheet sheet = workbook.getSheetAt(1);
 
 		// Definindo a linha que será lida pelo java
-		HSSFRow row = sheet.getRow(0);
+		HSSFRow row = sheet.getRow(linha);
+		// Definindo a celula que será lida
+		HSSFCell coluna = row.getCell(colunaA);
+		var numberOfRows = sheet.getPhysicalNumberOfRows();
 		
+		for(int i = 0; i <= numberOfRows - 1; i++) {
+			HSSFCell codProd = row.getCell(i);
+			var codProdF = codProd.getNumericCellValue();
+			planilhaList.add(codProdF);
+		}
+		
+		System.out.println(planilhaList.toString());
+		
+		
+		/*
 		var teste = sheet.getFirstRowNum();
-		
-		
-		
-		
 		var coluna = sheet.getColumnBreaks();
 		var colunas = sheet.getColumnOutlineLevel(1);
 		var colunaBrak = sheet.getColumnBreaks();
-		var numberOfRows = sheet.getPhysicalNumberOfRows();
-
-		// Definindo a celula que será lida
-		HSSFCell codProdO = row.getCell(1);
 		var codProdF = codProdO.getStringCellValue();
-		
 		System.out.println("O valor achado é esse --> " + codProdF);
-
-		
 		System.out.println(data);
 		System.out.println(fileName);
 		System.out.println("coluna --> " + coluna);
@@ -75,6 +78,7 @@ public class teste {
 		System.out.println("colunaBrak --> " + colunaBrak);
 		System.out.println("numberOfRows --> " + numberOfRows);
 		System.out.println("teste --> " + teste);
+		*/
 		
 		
 		List<Integer> A = Arrays.asList(0,0);
@@ -83,21 +87,11 @@ public class teste {
 		List<Integer> D = Arrays.asList(0,C.get(1)+1);
 		List<Integer> E = Arrays.asList(0,D.get(1)+1);
 		
+		
 		System.out.println("A -> " + A.toString() + "  B -> " + B.toString() + " C -> " + C.toString());
 		
 
 	}
-	
-	
-	
-	
-	/*
-	public int obterNumeroDeCelulas() {
-		int contatador = 0;
-		
-		
-	}
-	*/
-	
+
 
 }
